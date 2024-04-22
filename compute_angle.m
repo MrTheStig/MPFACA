198ca531ef6730b03a368899dcf8faf6316478a0
Zhou Li <zhouli@ctgu.edu.cn>
2024-04-22 19:06:28 +0800

    Update compute_angle.m

--- compute_angle.m
+++ compute_angle.m
@@ -1,9 +1,9 @@
-function angles=compute_angle(pos,poses,n)%angles 是引力，斥力与x轴的角度向量,pos 是起点坐标，poses 是目标或障碍的坐标向量,取前n行进行计算
+function angles=compute_angle(pos,poses,n)
 deltaX = ones(n);
 deltaY = ones(n);
 r = ones(n);
 angles = ones(n);
-for i=1:n%n 是计算数目
+for i=1:n
     deltaX(i)=poses(i,1)-pos(1);
     deltaY(i)=poses(i,2)-pos(2);
     r(i)=sqrt(deltaX(i)^2+deltaY(i)^2);
@@ -12,6 +12,6 @@ for i=1:n%n 是计算数目
     else
         theta=-1*acos(deltaX(i)/r(i));
     end
-    angles(i)=theta;% 保存每个角度在Y 向量里面，第一个元素是与目标的角度，后面都是与障碍的角度
+    angles(i)=theta;
 end
 end
