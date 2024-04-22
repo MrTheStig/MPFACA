a2095cd8c9f14ff900492b54ee21bf6ac3d204d0
Zhou Li <zhouli@ctgu.edu.cn>
2024-04-22 19:07:43 +0800

    Update load_map_info.m

--- load_map_info.m
+++ load_map_info.m
@@ -1,30 +1,24 @@
 function [mapsize,start_pos,goal_pos,obs_info] = load_map_info(path)
-%load_map_info 从txt读取地图信息
+%load_map_info 
 mapinfo = fopen(path);
 lineinfo = fgetl(mapinfo);
 lineid =0;
 while ischar(lineinfo)
     if lineid == 0
-        %读取地图大小
         mapsize = getlineinfo(lineinfo);
     elseif lineid == 1
-        %读取起点终点
         data = getlineinfo(lineinfo);
         start_pos = data(1:2);
         goal_pos = data(3:4);
     elseif lineid == 2
-        %读取障碍物个数
         %data = getlineinfo(lineinfo);
         obs_num = str2num(lineinfo);
-        %初始化障碍物
         obs_info = zeros(obs_num,4);
     elseif lineid > 2 && lineid <= (2+obs_num)
-        %读取障碍物信息
         data = getlineinfo(lineinfo);
         obs_info(lineid-2,:)=data;
     end
-    lineinfo = fgetl(mapinfo);%读取下一行
+    lineinfo = fgetl(mapinfo);
     lineid = lineid + 1;
 end
 end
-
