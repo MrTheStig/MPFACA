539b854d8344d5ca6b905666985e6bdae8125583
Zhou Li <zhouli@ctgu.edu.cn>
2024-04-22 19:07:23 +0800

    Update getlineinfo.m

--- getlineinfo.m
+++ getlineinfo.m
@@ -1,8 +1,8 @@
 function [ data ] = getlineinfo( lineinfo )
-%getlineinfo 将txt读取的行信息从字符串转换成矩阵
-s=regexp(lineinfo,'\s+');%按空格分隔
+%getlineinfo 
+s=regexp(lineinfo,'\s+');
 data=zeros(length(s)+1,1);
-for i=1:length(s)   % 将字符串全部转为数组, 存于data中
+for i=1:length(s)   
     if i==1
         data(i)=str2num(lineinfo(1:s(i)));   
     else
